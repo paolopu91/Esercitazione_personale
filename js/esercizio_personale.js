bac5c2967f78cf8d8eb10e09a0f19dad64888500
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-20 15:59:56 +0200

    aggiunti esempi e modifiche su variabili

---
+++ js/esercizio_personale.js
@@ -0,0 +1,48 @@
+
+
+console.log("-----------------")
+const nameBtn = document.querySelector(".btn")
+
+nameBtn.addEventListener("click" , function() {
+
+    let nameBtn = prompt ("Scrivi qui il tuo nome")
+    nameBtn.innerHTML =
+ });
+
+console.log("--------------------------------")
+// let userName = prompt ("Scrivi qui il tuo nome")
+// const nameBtn = document.querySelector(".btn")
+
+console.log("-----------------------------")
+// dichiariamo una variabile
+// le variabili sono const, let, var
+
+
+const nomeUtente = 'Paolo';
+const cognomeUtente = 'Pugliese';
+// non si possono cambiare e quindi non si può dare un'altro valore alle costranti
+ 
+
+let nameUtente = 'Matteo';
+let surnameUtente = 'Pugliese';
+// con il let essendo una variabile possiamo riassegnare alla nostra variabile(nameUtente) un'altro valore
+nameUtente = 'Valerio';
+
+
+// Valori booleani sono true o false
+
+console.log("------------------------")
+
+// variabili con i colori(volevo creare un array ma non ci sono riuscito)
+
+// let coloriBlu = ['blu']
+// let coloriNero = ['nero']
+// let coloreVerde = ['verde']
+
+
+// le variabili sono case sensisitive
+// possiamo usare numeri 0-infinito
+// possiamo usare lettere a-z
+
+console.log(nomeUtente)
+console.log(cognomeUtente)
