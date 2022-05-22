df86954a0e552cb1404ea2eeff81cdf082fb90b6
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-22 13:03:47 +0200

    aggiunte modifiche spiegazione

--- js/tabelline.js
+++ js/tabelline.js
@@ -3,9 +3,6 @@
 const btnCalcola = document.getElementById("btn-calcola-tabellina")
 const inputMoltiplicatore = document.querySelector("[name='moltiplicatore']")
 
-console.dir(btnCalcola)
-console.dir(inputMoltiplicatore)
-
 btnCalcola.addEventListener("click", function () {
     // controllo che il ciclo funzioni
     console.log("Pulsante calcola cliccato")
@@ -17,23 +14,28 @@ btnCalcola.addEventListener("click", function () {
 
     // generiamo la tabellina
 
-    const resultListHtml= document.querySelector(".result-list")
+    let resultListHtml= document.querySelector(".result-list")
 
     // Questo risultato serve per far scomparire i risultati precedenti all'interno del form
+    
+    // in pratica usiamo il nostro innerHTML senza niente tra le virgolette
+    // per resettare ad ogni cambio di tabellina il campo, in modo da non sovrapporre o meglio
+    // non aggiungere una lista infinita di numeri
+
     resultListHtml.innerHTML = "";
 
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 1; i <= 10; i++) {
        const risultato = moltiplicatore * i;
 
     //  stampiamo a schermo il risultato della nostra moltiplicazione
 
-        const testo = `${moltiplicatore} * ${i} = ${risultato}`
+        const number = `${moltiplicatore} * ${i} = ${risultato}`
 
-        console.log(testo)
+        console.log(number)
 
         // aggiungere += se vogliamo vedere tutta la tabellina scritta
-        // se mettiamo solo = vedremo solo l'ultimo numero del ciclo perchè tutti gli altri risultati si sovrascrivono
-        resultListHtml.innerHTML += `<li>${testo}</li>`
+        // se mettiamo solo = vedremo solo l'ultimo numero del ciclo perchè tutti gli altri risultati si sovrascrivono in un lasso di tempo brevissimo
+        resultListHtml.innerHTML += `<li>${number}</li>`
 
     }
     })
